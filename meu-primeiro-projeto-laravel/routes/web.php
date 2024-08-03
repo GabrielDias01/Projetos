8bdde2624ec6bc4cb6609a5cf74d6b6dd070ab03
GabrielDias01 <gabrieldiassr@gmail.com>
2024-08-02 21:47:08 -0300

    0208

--- meu-primeiro-projeto-laravel/routes/web.php
+++ meu-primeiro-projeto-laravel/routes/web.php
@@ -1,13 +1,14 @@
 <?php
 
+use App\Http\Controllers\ProdutoController;
+use App\Models\Produto;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view ('home');
 });
-Route::get('/products', function () {
-    return view ('products');
-});
+Route::get('/products', [ProdutoController::class,'index'] ) ;
+
 Route::get('/contacts', function () {
     return view ('contacts');
 });
