167e49f5f1ce084d9b021b7e739db16b593c9615
GabrielDias01 <gabrieldiassr@gmail.com>
2024-08-14 21:55:10 -0300

    1408

---
+++ LojaFerramentas/resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+     <a href="/login"><h2>Login</h2></a> <br>
+     <a href="/registro"><h2>Register</h2></a> <br>
+</div>
+
+@endsection
